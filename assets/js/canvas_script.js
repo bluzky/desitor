fa69d1bd1c83af69e24f0f96e173f53800484f68
Dung Nguyen <bluesky.1289@gmail.com>
2019-01-05 08:27:41 +0700

    tst

--- assets/js/canvas_script.js
+++ assets/js/canvas_script.js
@@ -1,1 +1,42 @@
+import paper from 'paper';
+
+paper.install(window);
+// Keep global references to both tools, so the HTML
+// links below can access them.
+var tool1, tool2, reset;
+
+window.onload = function() {
+	paper.setup('drawing-board');
+
+	// Create two drawing tools.
+	// tool1 will draw straight lines,
+	// tool2 will draw clouds.
+
+	// Both share the mouseDown event:
+	var path;
+
+	function onMouseDown(event) {
+	  path = new Path();
+	  path.strokeColor = 'rebeccaPurple';
+    path.strokeWidth = 3;
+	  path.add(event.point);
+	}
+
+	tool1 = new Tool();
+	tool1.onMouseDown = onMouseDown;
+
+	tool1.onMouseDrag = function(event) {
+	  path.add(event.point);
+	}
+
+	tool2 = new Tool();
+	tool2.minDistance = 20;
+	tool2.onMouseDown = onMouseDown;
+
+	tool2.onMouseDrag = function(event) {
+	  // Use the arcTo command to draw cloudy lines
+	  path.arcTo(event.point);
+	}
+}
+paper.view.draw();
 
